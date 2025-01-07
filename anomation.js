7d54a6644d8d91e8d858e10e1118d65011750741
Datta padre <dattapadre7249@gmail.com>
2025-01-07 19:29:51 +0530

    Add files via upload

---
+++ anomation.js
@@ -0,0 +1,42 @@
+gsap.from(".logo",{
+    y:-40,
+    duration:1,
+    opacity:0
+})
+
+gsap.from(".s1part2 img",{
+    duration:2,
+    opacity:0,
+    scale:0.5
+
+})
+gsap.from("h1,h2,h3",{
+    y:50,
+    duration:1,
+    stagger:0.5,
+    opacity:0
+
+})
+
+gsap.from("#link",{
+    x:-50,
+    duration:2,
+    opacity:0,
+    stagger:-0.3
+})
+
+gsap.from(".section1 p",{
+    y:40,
+    duration:1,
+    opacity:0,
+    sragger:0.3
+
+})
+gsap.from(".menu",{
+    y:-40,
+    duration:1,
+    opacity:0,
+    sragger:0.3,
+    zIndex:999
+
+})
